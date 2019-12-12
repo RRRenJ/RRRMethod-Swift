31b2b34969c22e0dc9bf38aed69cdcdeaeefe5f2
renjing <rrrenj@outlook.com>
2019-12-12 11:27:33 +0800

    0.0.8

--- RRRMethod-SwiftDemo/Pods/Target Support Files/Pods-RRRMethod-SwiftDemo/Pods-RRRMethod-SwiftDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RRRMethod_SwiftDemo : NSObject
-@end
-@implementation PodsDummy_Pods_RRRMethod_SwiftDemo
-@end
--- RRRMethod-SwiftDemo/Pods/Target Support Files/RRRExtension/RRRExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RRRExtension : NSObject
-@end
-@implementation PodsDummy_RRRExtension
-@end
